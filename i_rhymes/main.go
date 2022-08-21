1726c3af6ff683c6169a4827d04db2eeaed329f7
amdf <787535+amdf@users.noreply.github.com>
2022-08-21 21:31:45 +0300

    trie недоделано

--- i_rhymes/main.go
+++ i_rhymes/main.go
@@ -2,87 +2,147 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"fmt"
 	"io"
 	"os"
 	"strconv"
-	"sync"
+	"strings"
 	"time"
 )
 
+var debug bool
+
+func reverse(s string) string {
+	var byte strings.Builder
+	byte.Grow(len(s))
+	for i := len(s) - 1; i >= 0; i-- {
+		byte.WriteByte(s[i])
+	}
+	return byte.String()
+}
+
+//Node represent each character
+type Node struct {
+	//this is a single letter stored for example letter a,b,c,d,etc
+	Char string
+	//store all children  of a node
+	//that is from a-z
+	//a slice of Nodes(and each child will also have 26 children)
+	Children [26]*Node
+}
+
+/// NewNode this will be used to initialize a new node with 26 children
+///each child should first be initialized to nil
+func NewNode(char string) *Node {
+	node := &Node{Char: char}
+	for i := 0; i < 26; i++ {
+		node.Children[i] = nil
+	}
+	return node
+}
+
+// Trie  is our actual tree that will hold all of our nodes
+//the Root node will be nil
+type Trie struct {
+	RootNode *Node
+}
+
+// NewTrie Creates a new trie with a root('constructor')
+func NewTrie() *Trie {
+	//we will not use this node so
+	//it can be anything
+	root := NewNode("\000")
+	return &Trie{RootNode: root}
+}
+
+/// Insert inserts a word to the trie
+func (t *Trie) Insert(word string) {
+	///this will keep track of our current node
+	///when transversing our tree
+	///it should always start at the top of our tree
+	///i.e our root
+	current := t.RootNode
+
+	strippedWord := reverse(word)
+	for i := 0; i < len(strippedWord); i++ {
+		//from the ascii table a represent decimal number 97
+		//from the ascii table b represent decimal number 98
+		//from the ascii table c represent decimal number 99
+		/// and so on so basically if we were to say  98-97=1 which means the index of b is 1 and for  c is 99-97
+		///that what is happening below (we are taking the decimal representation of a character and subtracting decimal representation of a)
+		index := strippedWord[i] - 'a'
+		///check if current already has a node created at our current node
+		//if not create the node
+		if current.Children[index] == nil {
+			current.Children[index] = NewNode(string(strippedWord[i]))
+		}
+		current = current.Children[index]
+		//since we want to support autocomplete
+	}
+
+}
+
+func (t *Trie) SearchWord(word string) (result string) {
+	if debug {
+		fmt.Println("Searching", word)
+	}
+	strippedWord := reverse(word)
+
+	current := t.RootNode
+
+	var b strings.Builder
+
+	treeEnd := false
+	i := 0
+	for !treeEnd {
+		index := byte(0)
+		if i < len(strippedWord) {
+			index = strippedWord[i] - 'a'
+			i++
+		}
+
+		found := false
+
+		if nil == current.Children[index] {
+			for j := byte(0); j < 26; j++ {
+				if nil != current.Children[j] {
+
+					b.WriteByte('a' + j)
+					current = current.Children[j]
+
+					found = true
+					break
+				}
+			}
+		} else {
+			b.WriteByte('a' + index)
+			current = current.Children[index]
+
+			found = true
+		}
+
+		treeEnd = !found
+	}
+
+	result = reverse(b.String())
+
+	if debug && result == word {
+		fmt.Println("Searching", word, " got ", result)
+	}
+
+	for i := 0; (result == word) && i < len(dict_s); i++ {
+		result = dict_s[i]
+	}
+
+	return
+}
+
 var dictSize, wordCount int64
-var threads, uniqwords int
-var result map[word]string
+var threads int
 
-type word [10]byte
+//uniqwords int
 
-func (w *word) set(s string) {
-	if len(s) <= 10 {
-		i := 10 - len(s)
-		copy(w[:i], "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"[:i])
-		copy(w[i:], s)
-	} else {
-		panic("length")
-	}
-}
-
-func (w *word) maxrhyme() (result int) {
-	result = (10 - bytes.LastIndex(w[:], []byte{0x00})) - 2
-	return
-}
-
-var dict_w []word
 var dict_s []string
-
-func rhymeValue(s1, s2 word) (result int, equal bool) {
-	equal = true
-	for i := 9; i >= 0; i-- {
-		if 0 == s1[i] || 0 == s2[i] || s1[i] != s2[i] {
-			if !(0 == s1[i] && 0 == s2[i]) {
-				equal = false
-			}
-
-			break
-		}
-
-		result++
-
-	}
-
-	return
-}
-
-func maxRhymeWord(s word) (result string) {
-	var max int
-	var found, found2 string
-	// rmax := s.maxrhyme()
-
-	for i := range dict_s {
-		val, eq := rhymeValue(s, dict_w[i])
-		if !eq {
-			found2 = dict_s[i]
-			if val > max {
-				max = val
-				found = dict_s[i]
-				// if max == rmax {
-				// 	break
-				// }
-			}
-		}
-	}
-
-	result = found
-	if 0 == len(result) {
-		result = found2
-	}
-
-	if 0 == len(found) && 0 == len(found2) {
-		panic("!")
-	}
-
-	return
-}
 
 func processing(r io.Reader, w io.Writer) {
 	sc := bufio.NewScanner(r)
@@ -91,17 +151,16 @@ func processing(r io.Reader, w io.Writer) {
 		return
 	}
 
+	tr := NewTrie()
 	dictSize, _ = strconv.ParseInt(sc.Text(), 10, 64)
 
 	for i := 0; i < int(dictSize); i++ {
 		if !sc.Scan() {
 			break
 		}
-		t := sc.Text()
-		var w word
-		w.set(t)
-		dict_w = append(dict_w, w)
-		dict_s = append(dict_s, t)
+		s := sc.Text()
+		tr.Insert(s)
+		dict_s = append(dict_s, s)
 	}
 
 	if !sc.Scan() {
@@ -109,83 +168,90 @@ func processing(r io.Reader, w io.Writer) {
 	}
 	wordCount, _ = strconv.ParseInt(sc.Text(), 10, 64)
 
-	words := make([]word, wordCount)
-
 	uniqw := make(map[string]struct{})
+	words := make([]string, wordCount)
 
 	for i := 0; i < int(wordCount); i++ {
 		if !sc.Scan() {
 			break
 		}
 		s := sc.Text()
-		words[i].set(s)
+
+		words[i] = s
 		uniqw[s] = struct{}{}
 	}
 
-	uniqwords = len(uniqw)
+	res := make([]string, wordCount)
+	for i := range words {
+		res[i] = tr.SearchWord(words[i])
+	}
 
-	result = make(map[word]string)
-	res := parallel(words, result)
+	// uniqwords = len(uniqw)
+
+	// res := parallel(words, result)
 
 	for i := range res {
 		fmt.Fprintln(w, res[i])
 	}
-
 }
 
-func parallel(words []word, result map[word]string) []string {
-	var mut sync.RWMutex
-	var mut2 sync.Mutex
+// func parallel(words []word, result map[word]string) []string {
+// 	var mut sync.RWMutex
+// 	var mut2 sync.Mutex
 
-	r := make([]string, len(words))
+// 	r := make([]string, len(words))
 
-	proc := func(wg *sync.WaitGroup, part []word, res []string) {
+// 	proc := func(wg *sync.WaitGroup, part []word, res []string) {
 
-		for i := range part {
-			mut.RLock()
-			s, ok := result[part[i]]
-			mut.RUnlock()
-			if !ok {
-				s = maxRhymeWord(part[i])
-				mut.Lock()
-				result[part[i]] = s
-				mut.Unlock()
-			}
-			mut2.Lock()
-			res[i] = s
-			mut2.Unlock()
-		}
-		wg.Done()
-	}
-	var wg sync.WaitGroup
+// 		for i := range part {
+// 			mut.RLock()
+// 			s, ok := result[part[i]]
+// 			mut.RUnlock()
+// 			if !ok {
+// 				s = maxRhymeWord(part[i])
+// 				mut.Lock()
+// 				result[part[i]] = s
+// 				mut.Unlock()
+// 			}
+// 			mut2.Lock()
+// 			res[i] = s
+// 			mut2.Unlock()
+// 		}
+// 		wg.Done()
+// 	}
+// 	var wg sync.WaitGroup
 
-	packsize := len(words) / 4
+// 	packsize := len(words) / 4
 
-	for endOffset := len(words); endOffset > 0; endOffset -= packsize {
-		beginOffset := endOffset - packsize
-		if beginOffset < 0 {
-			beginOffset = 0
-		}
-		wg.Add(1)
-		threads++
-		go proc(&wg, words[beginOffset:endOffset], r[beginOffset:endOffset])
-	}
+// 	for endOffset := len(words); endOffset > 0; endOffset -= packsize {
+// 		beginOffset := endOffset - packsize
+// 		if beginOffset < 0 {
+// 			beginOffset = 0
+// 		}
+// 		wg.Add(1)
+// 		threads++
+// 		go proc(&wg, words[beginOffset:endOffset], r[beginOffset:endOffset])
+// 	}
 
-	wg.Wait()
+// 	wg.Wait()
 
-	return r
-}
+// 	return r
+// }
 
 func main() {
 	t := time.Now()
+	if len(os.Args) > 1 {
+		debug = true
+	}
 	processing(os.Stdin, os.Stdout)
+
 	if len(os.Args) > 1 {
 		fmt.Println()
 		fmt.Println(time.Since(t))
 		fmt.Printf("threads: %v\n", threads)
 		fmt.Printf("dictSize: %v\n", dictSize)
 		fmt.Printf("wordCount: %v\n", wordCount)
-		fmt.Printf("uniqwords: %v\n", uniqwords)
-		fmt.Printf("result size: %v\n", len(result))
+		// fmt.Printf("uniqwords: %v\n", uniqwords)
+		// fmt.Printf("result size: %v\n", len(result))
 	}
 }
--- i_rhymes/main_test.go
+++ i_rhymes/main_test.go
@@ -1,110 +1,17 @@
 package main
 
-import (
-	"bytes"
-	"testing"
-)
+import "testing"
 
-func TestMax(t *testing.T) {
-	var s word
+func TestAb(t *testing.T) {
 
-	s.set("id")
+	tr := NewTrie()
+	tr.Insert("ve")
+	tr.Insert("neeevzzv")
 
-	if 1 != s.maxrhyme() {
-		t.Error("TestMax wrong")
-	}
-
-	s.set("abc")
-
-	if 2 != s.maxrhyme() {
-		t.Error("TestMax wrong")
-	}
-
-	s.set("aaaaabbbbb")
-
-	if 9 != s.maxrhyme() {
-		t.Error("TestMax wrong")
-	}
-}
-
-func TestNorm1(t *testing.T) {
-	var buf, outbuf bytes.Buffer
-	buf.WriteString(
-		"3\n" +
-			"task\n" +
-			"decide\n" +
-			"id\n" +
-			"6\n" +
-			"flask\n" +
-			"code\n" +
-			"void\n" +
-			"forces\n" +
-			"id\n" +
-			"ask\n")
-
-	processing(&buf, &outbuf)
-
-	str := outbuf.String()
-
-	if 0 == len(str) {
-		t.Error("TestNorm1 wrong")
-	}
-
-}
-
-func TestRhymes(t *testing.T) {
-	var s1, s2 word
-
-	s1.set("task")
-	s2.set("flask")
-
-	if v, _ := rhymeValue(s1, s2); v != 3 {
-		t.Error("3 != task flask")
-	}
-
-	s1.set("decide")
-	s2.set("code")
-
-	if v, _ := rhymeValue(s1, s2); v != 2 {
-		t.Error("2 != decide code")
-	}
-
-	s1.set("id")
-	s2.set("void")
-
-	if v, _ := rhymeValue(s1, s2); v != 2 {
-		t.Error("2 != id void")
-	}
-
-	s1.set("code")
-	s2.set("forces")
-
-	if v, _ := rhymeValue(s1, s2); v != 0 {
-		t.Error("0 != code forces")
-	}
-}
-
-func TestEqual(t *testing.T) {
-	var s1, s2 word
-
-	s1.set("id")
-	s2.set("id")
-
-	if v, eq := rhymeValue(s1, s2); v != 2 || eq != true {
-		t.Error("id equals")
-	}
-
-	s1.set("flaskflask")
-	s2.set("flaskflask")
-
-	if v, eq := rhymeValue(s1, s2); v != 10 || eq != true {
-		t.Error("equals1")
-	}
-
-	s1.set("laskflask")
-	s2.set("flaskflask")
-
-	if v, eq := rhymeValue(s1, s2); v != 9 || eq != false {
-		t.Error("equals2")
-	}
+	tr.SearchWord("v")          //neeevzzv
+	tr.SearchWord("vnveneezve") //ve
+	tr.SearchWord("evzz")
+	tr.SearchWord("ezenne") //ve
+	tr.SearchWord("nneenn")
+	//t.Error()
 }
