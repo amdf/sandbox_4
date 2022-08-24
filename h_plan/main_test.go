a41a73040eef6ef21ec453778367229ec797bc35
amdf <787535+amdf@users.noreply.github.com>
2022-08-24 13:40:29 +0300

    добавил дерево

--- h_plan/main_test.go
+++ h_plan/main_test.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestXxx(t *testing.T) {
+	tree := NewTree(func(a, b Item) int { return a.(int) - b.(int) })
+	tree.Insert(1)
+	tree.Insert(2)
+	tree.Insert(5)
+	tree.Insert(10)
+
+	var iter Iterator
+	iter = tree.Min()
+	min := iter.Item()
+	iter = tree.Max()
+	max := iter.Item()
+
+	tree.DeleteWithKey(10)
+
+	iter = tree.Max()
+	max = 0
+	max = iter.Item()
+
+	if 0 == min || 0 == max {
+		t.Error()
+	}
+}
